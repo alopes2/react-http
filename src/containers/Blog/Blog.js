7e94e4e78d160c66f1f10cd095e76232f2044c52
alopes2 <andre.lopes@exxonmobil.com>
2018-01-26 14:44:40 -0200

    outputing list of posts with title

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,21 +7,26 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts: []
+    };
+
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
+                this.setState({posts: response.data});
                 console.log(response);
             })
             .catch(error => console.log(error));
     }
 
     render () {
+        const posts = this.state.posts
+            .map(post => <Post key={post.id} title={post.title}/>)
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
