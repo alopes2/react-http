842536561adbfda9dbb9eab6f6048d492fff3ac8
alopes2 <andre.lopes@exxonmobil.com>
2018-01-26 14:47:37 -0200

    transforming data and adding author

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -14,7 +14,14 @@ class Blog extends Component {
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                this.setState({posts: response.data});
+                const posts = response.data.slice(0,4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Andre'
+                    }
+                });
+                this.setState({posts: updatedPosts});
                 console.log(response);
             })
             .catch(error => console.log(error));
@@ -22,7 +29,10 @@ class Blog extends Component {
 
     render () {
         const posts = this.state.posts
-            .map(post => <Post key={post.id} title={post.title}/>)
+            .map(post => <Post 
+                            key={post.id} 
+                            title={post.title}
+                            author={post.author}/>);
         return (
             <div>
                 <section className="Posts">
